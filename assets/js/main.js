2df68e2b10bf2449e9f98e78cf9543c4480d6c07
Mohit Varshney <mohit.varshney@rapidshyp.com>
2024-02-28 10:56:31 +0530

    modal form and UI changes done on 27 feb 2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,65 @@
 $(window).on("scroll", function() {
     if($(window).scrollTop() > 50) {
-        $(".header").addClass("active");
+        $(".header").addClass("active-head");
     } else {
         //remove the background property so it comes transparent again (defined in your css)
-       $(".header").removeClass("active");
+       $(".header").removeClass("active-head");
     }
+});
+
+
+$(document).ready(function(){
+    $(".close").click(function(){
+        $('#joinWaitlistModal').hide()
+        location.reload(true);
+    })
+    $("#submit-button").submit(function(e){
+        var email = $("#useremail").val();
+        console.log(email);
+        $("#joinWaitlistModal").css('display','flex');
+        $("#email").val(email);
+        return false;
+    });  
+    $('.php-email-form').on('submit', function (e) {
+        e.preventDefault();
+
+        var formData = $(this).serializeArray();
+        var data = {};
+        formData.forEach(function (item) {
+            data[item.name] = item.value;
+        });
+
+        var json = JSON.stringify(data);
+        console.log(json);
+
+
+        $('.loading').show();
+        $('.error-message').hide();
+        $('.sent-message').hide();
+
+        $.ajax({
+            url: 'https://www.rapidshyp.com/webhooks/receive_lead.php',
+            type: 'POST',
+            data: json,
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (response) {
+                if (response.success) {
+                    $('.sent-message').text(response.message);
+                    $('.sent-message').show();
+                    $('.php-email-form').trigger('reset'); // Reset the form on success
+                } else {
+                    $('.error-message').text(response.message);
+                    $('.error-message').show();
+                }
+                $('.loading').hide();
+            },
+            error: function (xhr, status, error) {
+                // $('.error-message').text("An error occurred: " + xhr.responseText);
+                $('.error-message').show();
+                $('.loading').hide();
+            }
+        });
+    });  
+
 });
--- index.php
+++ index.php
@@ -54,8 +54,10 @@
                             <p class="description">Get a bird's eye view with our customizable dashboard. Stay on top of things! <br> Revamp your work process with our game-changing feature.</p>
                         </div>
                         <div class="join-us">
-                            <input type="text" placeholder="Type Your Email Here">
-                            <button class="solid-action-btn">Join The Waitlist</button>
+                            <form action="#" id="submit-button">
+                                <input type="text" placeholder="Type Your Email Here" id="useremail" required>
+                                <button type="submit" class="solid-action-btn" id="openWaitlistForm">Join The Waitlist</button>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -88,7 +90,6 @@
                                             <div class="item">
                                                 <h5 class="head">Shipping Automation</h5>
                                                 <p class="body">Get a bird's eye view with our customizable dashboard. Get a bird's eye view with our customizable dashboard. Get a bird's eye view with our customizable dashboard. Get a bird's eye view with our customizable dashboard.</p>
-                                                <!-- <a href="#" class="footer">Explore The Feature <i class="fas fa-arrow-right"></i></a> -->
                                                 <div class="scroll-path">
                                                     <span class="progress-bar"></span>
                                                 </div>
@@ -99,7 +100,6 @@
                                             <div class="item">
                                                 <h5 class="head">Shipping Automation</h5>
                                                 <p class="body">Get a bird's eye view with our customizable dashboard. Get a bird's eye view with our customizable dashboard. Get a bird's eye view with our customizable dashboard. Get a bird's eye view with our customizable dashboard.</p>
-                                                <!-- <a href="#" class="footer">Explore The Feature <i class="fas fa-arrow-right"></i></a> -->
                                                 <div class="scroll-path">
                                                     <span class="progress-bar"></span>
                                                 </div>
@@ -110,7 +110,6 @@
                                             <div class="item">
                                                 <h5 class="head">Shipping Automation</h5>
                                                 <p class="body">Get a bird's eye view with our customizable dashboard. Get a bird's eye view with our customizable dashboard. Get a bird's eye view with our customizable dashboard. Get a bird's eye view with our customizable dashboard.</p>
-                                                <!-- <a href="#" class="footer">Explore The Feature <i class="fas fa-arrow-right"></i></a> -->
                                                 <div class="scroll-path">
                                                     <span class="progress-bar"></span>
                                                 </div>
@@ -623,12 +622,85 @@
             </div>
             <div class="row">
                 <div class="col-12">
-                    <img src="assets/images/footer-img.png" alt="">
                 </div>
             </div>
         </div>
+        <img src="assets/images/footer-img.png" alt="">
     </footer>
-  
+    
+    <div class="blur-overlay" id="joinWaitlistModal">
+        <div class="dialog-box">
+            <div class="head">
+                <h5>Join The Waitlist</h5>
+                <div class="close">
+                    <i class="fas fa-times"></i>
+                </div>
+            </div>
+            <div class="content" >
+                <div class="container">
+                    <div class="row mb-4">
+                        <div class="col-12">
+                        <?php $date=time();?>
+                            <form class="php-email-form" method="post">
+                                <input type="hidden" name="access_key" value="<?= hash_hmac('sha256', $date, 'cb39d436dc8dad92779a267dd5594144935e56fadb354b8a5b0e50919202ef741d67b0d11d6d83ee143db8d80be4d0f1') ?>">
+                                <input type="hidden" name="key" value="<?php echo $date ?>">
+
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="mb-3">
+                                            <label for="name" class="form-label">Name</label>
+                                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="mb-3">
+                                            <label for="company" class="form-label">Company Name</label>
+                                            <input type="text" class="form-control" name="company" id="company" placeholder="Enter Company Name" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="mb-3">
+                                            <label for="email" class="form-label">Email address</label>
+                                            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="mb-3">
+                                            <label for="phone" class="form-label">Mobile Number</label>
+                                            <input type="number" class="form-control" name="phone" id="phone" placeholder="Mobile Number" onKeyPress="if(this.value.length==10) return false;" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="mb-3">
+                                            <label for="count" class="form-label">How Many Shipment/Order do you have received in a Month?</label>
+                                            <select class="form-select" name="count" id="count" placeholder="How many orders do you ship every month?" required>
+                                                <option>How many orders do you ship every month?</option>
+                                                <option value="Setting up new Business">Setting up new Business</option>
+                                                <option value="Between 1 to 10 Orders">Between 1 to 10 Orders</option>
+                                                <option value="11 to 100 Orders">11 to 100 Orders</option>
+                                                <option value="101 to 500 Orders">101 to 500 Orders</option>
+                                                <option value="501 to 1000 Orders">501 to 1000 Orders</option>
+                                                <option value="1000 to 3000 Orders">1000 to 3000 Orders</option>
+                                                <option value="More than 3000 Orders">More than 3000 Orders</option>			
+                                                </select>
+                                        </div>
+                                    </div>
+                                    <div class="loading">Loading</div>
+                                    <div class="error-message">Sorry! Invalid Data</div>
+                                    <div class="sent-message">Thank you for showing Interest. We will reach out to you</div>
+                                </div>                               
+                                <div class="row align-items-center justify-content-center text-center">
+                                    <div class="col">
+                                        <button type="submit" class="solid-action-btn m-0" id="submitEmailForm">Get a Quotation</button>
+                                    </div>
+                                </div>
+                              </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="assets/js/main.js"></script>
 </body>
